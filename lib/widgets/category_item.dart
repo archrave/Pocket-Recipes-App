532c0b2870cf8ba356bbfb4316e155940fcb8730
Divyansh Joshi <divyanshjoshi20@gmail.com>
2021-07-17 20:24:28 +0530

    Added the meals screen, and the navigator to it

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String itemTitle;
@@ -6,21 +7,38 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem({this.itemTitle, this.itemColor});
 
+  void selectCategory(BuildContext ctx) {
+    // Navigator feature HELPS to navigate between screens
+    // Adds another screen on top of the screen before, like a stack
+    // Screens are practically alligned using stack
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return MealsScreen();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Text(itemTitle),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            itemColor.withOpacity(0.7),
-            itemColor,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    //Inkwell is basically a GestureDetector() but w a cool default animation
+    return InkWell(
+      //Forwarding build() methods context to the function used
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius:
+          BorderRadius.circular(15), //To match the shape of container below
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Text(itemTitle),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              itemColor.withOpacity(0.7),
+              itemColor,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
       ),
     );
   }
---
+++ lib/widgets/meals_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('the meals'),
+      ),
+      body: Container(),
+    );
+  }
+}
