fdedc69a991e4e3da73cf1fe816269e0f5623893
Divyansh Joshi <divyanshjoshi20@gmail.com>
2021-07-24 17:16:47 +0530

    Added the tabs bar at the bottom

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,31 +8,48 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _tabList = [
+    {'tab': CategoriesScreen(), 'title': 'Categories'},
+    {'tab': FavoritesScreen(), 'title': 'Favorites'},
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     //The following widget is used to put tabs right below the appbar
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Pocket Recipes"),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [CategoriesScreen(), FavoritesScreen()],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _tabList[_selectedPageIndex]['title'],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        //type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            //backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Catergories'),
+          ),
+          BottomNavigationBarItem(
+            //backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
+      ),
+      body: _tabList[_selectedPageIndex]['tab'],
     );
   }
 }
